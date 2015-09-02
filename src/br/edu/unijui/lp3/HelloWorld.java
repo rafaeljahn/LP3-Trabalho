a3c57deeb09a90bdded1f711be7b645e89b3fcf9
dener <dener@10.40.1.178>
2015-09-02 20:39:20 -0300

    Nada

--- src/br/edu/unijui/lp3/HelloWorld.java
+++ src/br/edu/unijui/lp3/HelloWorld.java
@@ -3,7 +3,7 @@ package br.edu.unijui.lp3;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Fuck off");
+		System.out.println("Fuck off Marilene");
 	}
 
 }
